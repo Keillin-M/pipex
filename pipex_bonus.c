e23900d2d4d11c2f0f8bde2c30c3ef3ac24941ed
Keillin Maeda <kmaeda@c4b2c4.42berlin.de>
2025-07-24 17:57:13 +0200

    bonus part2

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -12,22 +12,22 @@
 
 #include "pipex_bonus.h"
 
-static void	first_child(t_data *data, int i, char **envp)
+static void	first_child(t_data *data, char **envp)
 {
 	close(data->fd[0]);
 	dup2(data->infile, STDIN_FILENO);
 	dup2(data->fd[1], STDOUT_FILENO);
 	close(data->fd[1]);
-	if (execve(data->path1, data->cmd1, envp) == -1)
+	if (execve(data->path[0], data->cmds[0], envp) == -1)
 		error_exit(data, "command not found\n");
 }
 
-static void	last_child(t_data *data, int i, char **envp)
+static void	last_child(t_data *data, char **envp)
 {
 	dup2(data->prev_fd, STDIN_FILENO);
 	dup2(data->outfile, STDOUT_FILENO);
 	close(data->prev_fd);
-	if (execve(data->path2, data->cmd2, envp) == -1)
+	if (execve(data->path[data->cmd_count - 1], data->cmds[data->cmd_count - 1], envp) == -1)
 		error_exit(data, "command not found\n");
 }
 
@@ -44,30 +44,27 @@ static void	middle_child(t_data *data, int i, char **envp)
 
 void	pipex(t_data *data, pid_t *pid, int i, char **envp)
 {
+	if (i < data->cmd_count - 1 && pipe(data->fd) == -1)
+		error_exit(data, "pipe failed\n");
+	pid[i] = fork();
+	if (pid[i] == -1)
+		error_exit(data, "fork failed\n");
+	if (pid[i] == 0)
+	{
+		if (i == 0)
+			first_child(data, envp);
+		else if (i == data->cmd_count - 1)
+			last_child(data, envp);
+		else
+			middle_child(data, i, envp);
+	}
+	if (data->prev_fd != -1)
+		close(data->prev_fd);
 	if (i < data->cmd_count - 1)
-		{
-			if (pipe(data->fd) == -1)
-				error_exit(data, "pipe failed\n");
-		}
-		pid[i] = fork();
-		if (pid[i] == -1)
-			error_exit(data, "fork failed\n");
-		if (pid[i] == 0)
-		{
-			if (i == 0)
-				first_child(data, i, envp);
-			else if (i == data->cmd_count - 1)
-				last_child(data, i ,envp);
-			else
-				middle_child(data, i, envp);
-		}
-		if (data->prev_fd != -1)
-			close(data->prev_fd);
-		if (i < data->cmd_count - 1)
-		{
-			close(data->fd[1]);
-			data->prev_fd = data->fd[0];
-		}
+	{
+		close(data->fd[1]);
+		data->prev_fd = data->fd[0];
+	}
 }
 
 void	parent(t_data *data, char **envp)
@@ -77,6 +74,8 @@ void	parent(t_data *data, char **envp)
 
 	i = 0;
 	data->prev_fd = -1;
+	if(data->cmd_count <= 0)
+		error_exit(data, "no commands to execute\n");
 	pid = malloc(sizeof(pid_t) * data->cmd_count);
 	if (!pid)
 		error_exit(data, "malloc failed\n");
@@ -85,8 +84,8 @@ void	parent(t_data *data, char **envp)
 		pipex(data, &pid, i, envp);
 		i++;
 	}
-	if (prev_fd != -1)
-		close(prev_fd);
+	if (data->prev_fd != -1)
+		close(data->prev_fd);
 	i = 0;
 	while (i < data->cmd_count)
 	{
